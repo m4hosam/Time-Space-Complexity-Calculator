24c2426065504f935da4acbe950a9c823c0d62b8
Mohamed Hosam <79701103+m4hosam@users.noreply.github.com>
2022-03-22 21:28:45 +0300

    Print the complexity (Last Version)
    All parts have been added together and the program has been tested for different cases and it shoed success on the run. Of course it has limitations but it worked on a lot of cases.

--- main2.c
+++ main2.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "complexty.c"
 #define text "index.txt"
 
-// Stack Part
+// Stack vairables
 char arr[20];
 int top = 0;
 int elementNumber = 0;
+// complexity stack variables
+char bracket[20] = "";
+int bigO[20] = {0};
+int sumbigO[20][20] = {0};
+int pointer = 0;
+int totalelements = 0;
+int complextyAsInt;
+// String arrays are used for searching
 char opsCases[20][20] = {"++", "--", "+=", "-=", "/=", "*=", "+", "-", "/", "*"};
 char loops[20][20] = {"do", "while", "for", "{", "}", " "};
 char functionTypes[20][20] = {"int", "double", "char", "float", " "};
-char functionNames[20][20] = {""};
+// Finding the functions and store to the array
+char functionNames[50][50] = {""};
 int functionComplexty[20] = {1};
-int spaceComplexty[20] = {1};
+// Used for space complexity
+int spaceComplexty[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+int spacecounter = 0;
 int functionNumbers = 0;
-int startMainIndex;
+// getting the main index of main function
+int startMainIndex = 0;
 
+// bracket stack function
 int isEmpty()
 {
     if (elementNumber == 0)
@@ -72,7 +84,87 @@ int pop()
     elementNumber--;
     return data;
 }
-// End Of Stack Part
+
+// complexity Stack part
+
+int isEmptyStack()
+{
+    if (totalelements == 0)
+        return 0;
+    else
+        return 1;
+}
+
+int addToStack(char brack, int complex)
+{
+    if (totalelements - 1 >= 0)
+    {
+        if (bracket[totalelements - 1] != brack)
+        {
+
+            // some work
+            popFromStack();
+            return 1;
+        }
+        else
+        {
+            bracket[pointer] = brack;
+            bigO[pointer] = complex;
+            pointer++;
+            totalelements++;
+            return 0;
+        }
+    }
+    else
+    {
+        bracket[pointer] = brack;
+        bigO[pointer] = complex;
+        pointer++;
+        totalelements++;
+        return 0;
+    }
+}
+
+void popFromStack()
+{
+    if (!isEmptyStack())
+    {
+        printf("stack Empty");
+        return;
+    }
+    int largest = 0;
+    int i;
+    for (i = 0; i < 20; i++)
+    {
+        if (sumbigO[pointer - 1][i] > largest)
+        {
+            largest = sumbigO[pointer - 1][i];
+        }
+    }
+    int newbigO = bigO[pointer - 1];
+    if (largest != 0)
+    {
+        newbigO = bigO[pointer - 1] * largest;
+    }
+    pointer--;
+    totalelements--;
+    for (i = 0; i < 20; i++)
+    {
+        if (sumbigO[pointer - 1][i] == 0)
+        {
+            sumbigO[pointer - 1][i] = newbigO;
+            break;
+        }
+    }
+    if (!isEmptyStack())
+    {
+        complextyAsInt = sumbigO[pointer][0];
+    }
+    for (i = 0; i < 20; i++)
+    {
+        sumbigO[pointer][i] = 0;
+    }
+}
 
 // Help Method
 char doesContain(char buf[])
@@ -312,7 +404,7 @@ void findRecursive(int ptr, char functionName[])
 
         while (isThere != NULL)
         {
-            printf("Found in  index: %d\n\n", isThere - buf);
+            // printf("Found in  index: %d\n\n", isThere - buf);
             strcpy(recursiveLines[count], isThere);
             count++;
             isThere = strstr(isThere + 1, functionName);
@@ -328,12 +420,12 @@ void findRecursive(int ptr, char functionName[])
             if (recursiveLines[1][k] == '-')
             {
                 functionComplexty[functionNumbers - 1] = 10;
-                printf("O(n)\n");
+                // printf("O(n)\n");
             }
             if (recursiveLines[1][k] == '/' || recursiveLines[1][k] == '*')
             {
                 functionComplexty[functionNumbers - 1] = 3;
-                printf("O(logn)\n");
+                // printf("O(logn)\n");
             }
         }
     }
@@ -354,7 +446,7 @@ void findRecursive(int ptr, char functionName[])
     if (flag == 2)
     {
         functionComplexty[functionNumbers - 1] = 313;
-        printf("O(2^n)\n");
+        // printf("O(2^n)\n");
     }
 
     else if (flag == 3)
@@ -392,10 +484,10 @@ int searchForFunctions()
                 break;
         }
 
-        if (i == 0 && ptrmain != NULL) // int not main()
+        if (i == 0 && ptrmain != NULL) // int main()
         {
             startMainIndex = startIndex;
-            printf("No other Function Just main\n");
+            // printf("No other Function Just main\n");
             fclose(fileText);
             return 1;
         }
@@ -404,7 +496,7 @@ int searchForFunctions()
             int comp = strcmp(buf, ptr[i]);
             if (comp == 0)
             {
-                printf("We have a function %s\n", ptr[i]);
+                // printf("We have a function %s\n", ptr[i]);
                 int k, z;
                 for (k = 0; k < strlen(ptr[i]); k++)
                 {
@@ -423,18 +515,45 @@ int searchForFunctions()
                 functionNumbers++;
                 findRecursive(startIndex, functionName);
             }
-            // printf("ptr:|%s|\n", ptr[i]);
-            // printf("buf:|%s|\n", buf);
-
-            // printf("Function Name -%s- \n", functionName);
-        }
-        else
-        {
-            printf("not Interested\n");
         }
         startIndex = ftell(fileText);
     }
     fclose(fileText);
+}
+
+int getSpaceComplexity(char buf[])
+{
+    int k;
+    for (k = 0; k < strlen(buf); k++)
+    {
+        if (buf[k] == '[')
+        {
+            if (!isdigit(buf[k + 1]))
+            {
+                spaceComplexty[spacecounter] = spaceComplexty[spacecounter] * 10;
+                return getSpaceComplexity(buf + k + 1);
+            }
+        }
+        else if (buf[k] == ',')
+        {
+            spacecounter++;
+            return getSpaceComplexity(buf + k + 1);
+        }
+    }
+    spacecounter++;
+}
+
+int getSpaceComplexityTotal()
+{
+    int total = 0;
+    for (int i = 0; i < 20; i++)
+    {
+        if (spaceComplexty[i] > total)
+        {
+            total = spaceComplexty[i];
+        }
+    }
+    return total;
 }
 
 int getFileInfo()
@@ -453,80 +572,79 @@ int getFileInfo()
     int flag = 0;
     char *ptrs[20];
     char *ptrFunc[20];
-    int i, q, answer;
-    // printf("debug11\n");
+    char *ptrDataTypes[20];
+    int i, q, j, answer;
     while ((fgets(buf, 100, fileText)) != NULL)
     {
-        // printf("debug22\n");
+        // searching for loops in buf
         for (i = 0; i < 6; i++)
         {
             ptrs[i] = strstr(buf, loops[i]);
             if (ptrs[i] != NULL)
                 break;
         }
+        // searching for fanction calls at every line
         for (q = 0; q < functionNumbers; q++)
         {
             ptrFunc[q] = strstr(buf, functionNames[q]);
             if (ptrFunc[q] != NULL)
             {
-                printf("\n\nFunction Complextiy: %d\n\n", functionComplexty[q]);
+                flag++;
                 addToStack('{', functionComplexty[q]);
                 addToStack('}', 0);
+                spaceComplexty[spacecounter] = functionComplexty[q];
+                spacecounter++;
                 break;
             }
         }
-        // printf("debug33\n");
+        // search for space complexity
+        for (j = 0; j < 4; j++)
+        {
+            ptrDataTypes[j] = strstr(buf, functionTypes[j]);
+            if (ptrDataTypes[j] != NULL)
+            {
+                getSpaceComplexity(ptrDataTypes[j]);
+                break;
+            }
+        }
+
         char *semicolin = strstr(buf, ";");
         switch (i)
         {
         case 0:
             //  Do While loop
-            // printf("debug44\n");
-
+            flag++;
             answer = getDoWhileInfo(startIndex);
             addToStack('{', answer);
-            // printf("debug442\n");
-            // printf("\nDo While loop Found %d\n\n", answer);
             break;
 
         case 1:
             //  While loop
-            // printf("debug55\n");
             if (semicolin != NULL)
             {
                 addToStack('}', 0);
                 break;
             }
-
+            flag++;
             answer = getWhileInfo(0, startIndex);
             addToStack('{', answer);
-            // printf("debug552\n");
-            // printf("\n--While loop Found %d\n\n", answer);
             break;
         case 2:
             //  For loop
-            // printf("debug66\n");
+            flag++;
             answer = getForInfo(buf);
             addToStack('{', answer);
-            // printf("debug662\n");
-            // printf("\nFor loop Found %d\n\n", answer);
             break;
         case 3:
             //  { bracket
-            // printf("debug77\n");
             addToStack('{', 1);
-            // printf("\nbracket '{' Found\n\n");
             break;
         case 4:
             //  } bracket
-            // printf("debug88\n");
             addToStack('}', 0);
-            // printf("\nbracket '}' Found\n\n");
             break;
         case 5:
             //  not interested
-            // printf("debug99\n");
-            printf("\nNot Interested\n\n");
             break;
 
         default:
@@ -535,40 +653,41 @@ int getFileInfo()
 
         startIndex = ftell(fileText);
     }
+    if (flag == 0)
+    {
+        complextyAsInt = 1;
+    }
 
     fclose(fileText);
 }
 
-void getComplexity()
+char *getComplexity(int complexity)
 {
     int counter_n = 0;
     int counter_logn = 0;
     int counter_2n = 0;
-    char complex[100];
+    char *complex = malloc(100);
     // printf("comp:%d\n", complextyAsInt);
     // get n
-    if (complextyAsInt == 1)
+    if (complexity == 1)
     {
         strcpy(complex, "O(1)");
-        return;
+        return complex;
     }
-    while (complextyAsInt % 10 == 0)
+    while (complexity % 10 == 0)
     {
-        // printf("************");
         counter_n++;
-        complextyAsInt = complextyAsInt / 10;
+        complexity = complexity / 10;
     }
-    while (complextyAsInt % 3 == 0 && complextyAsInt != 1)
+    while (complexity % 3 == 0 && complexity != 1)
     {
-        // printf("************");
         counter_logn++;
-        complextyAsInt = complextyAsInt / 3;
+        complexity = complexity / 3;
     }
-    while (complextyAsInt % 313 == 0 && complextyAsInt != 1)
+    while (complexity % 313 == 0 && complexity != 1)
     {
-        // printf("************");
         counter_2n++;
-        complextyAsInt = complextyAsInt / 313;
+        complexity = complexity / 313;
     }
     if (counter_2n == 0)
     {
@@ -581,22 +700,22 @@ void getComplexity()
                 {
                     if (counter_logn == 1)
                     {
-                        sprintf(complex, "nlog(n)");
+                        sprintf(complex, "n*log(n)");
                     }
                     else
                     {
-                        sprintf(complex, "nlog(n)^%d", counter_logn);
+                        sprintf(complex, "n*log(n)^%d", counter_logn);
                     }
                 }
                 else
                 {
                     if (counter_logn == 1)
                     {
-                        sprintf(complex, "n^%dlog(n)", counter_n);
+                        sprintf(complex, "n^%d*log(n)", counter_n);
                     }
                     else
                     {
-                        sprintf(complex, "n^%dlog(n)^%d", counter_n, counter_logn);
+                        sprintf(complex, "n^%d*log(n)^%d", counter_n, counter_logn);
                     }
                 }
             }
@@ -612,19 +731,38 @@ void getComplexity()
                 }
             }
         }
+        else
+        {
+            if (counter_logn == 1)
+            {
+                sprintf(complex, "log(n)");
+            }
+            else
+            {
+                sprintf(complex, "log(n)^%d", counter_logn);
+            }
+        }
     }
     // get logn
 
-    printf("\n--------------%s-------------\n", complex);
+    return complex;
 }
 
 int main()
 {
-    char input[20] = "   This is my name";
     searchForFunctions();
     getFileInfo();
-    printf("\ntotal complexty: %d\n", complextyAsInt);
-    getComplexity();
+
+    int spaceComp = getSpaceComplexityTotal();
+    if (startMainIndex != 0)
+    {
+        printf("---space Complexity: %s\n\n", getComplexity(spaceComp));
+        printf("---Time Complexity: %s\n\n", getComplexity(complextyAsInt));
+    }
+    else
+    {
+        printf("Error No Main function Found\n\n");
+    }
 
     return 0;
 }
